1f180f565f51c983ea6bff3d85fed7a4ab154575
jasonchilcott <jasonchilcottcode@gmail.com>
2020-09-29 23:40:12 -0400

    changed user forms and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ApplicationRecord
   has_many :billings
   has_many :types, through: :billings
   has_secure_password
+  before_save :downcase_email
 
   validates :email, uniqueness:true
+
+  def downcase_email
+    self.email.downcase!
+  end
 end
